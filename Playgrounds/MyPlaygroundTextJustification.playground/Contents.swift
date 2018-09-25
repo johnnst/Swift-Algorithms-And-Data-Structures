c6445a24fd2851ab431950df8835ee40cabc217e
John Ngoi <ngoi.john@gmail.com>
2018-09-24 23:36:27 -0700

    updated MyPlaygroundTextJustification.playground

--- Playgrounds/MyPlaygroundTextJustification.playground/Contents.swift
+++ Playgrounds/MyPlaygroundTextJustification.playground/Contents.swift
@@ -1,6 +1,11 @@
 // John Ngoi
 // Text justification
 // Given a string of words and a max length, L, format the text such that each line has exactly L characters and is fully left and right justified.
+// Restrictions:
+// You need to fit as many words in every line.
+// You are not allowed to split a word and put them in next line.
+// Pad extra spaces if needed so that each line has L characters.
+// Extra spaces between words should be distributed as evenly as possible.
 // Example ...
 // words = ["This", "is", "an", "example", "of", "text", "justification."]
 // line length = 16
@@ -8,86 +13,89 @@
 // ["This    is    an",
 // "example  of text",
 // "justification.  "]
-// 
 //
-
-import UIKit
 
 func textJustification (words: [String], maxLength: Int) -> [String]? {
     var results = [String]() // store the results of each line
-    
+
     if words.count == 0 || maxLength == 0 {
         return results
     }
-    
-    var count = 0 // counter to keep track of total words length per line
+
+    var lineCharacterCount = 0 // counter to keep track of number of characters per line
     var lineIndex = 0 // index tracker to start each new line from the words array
-    
-    for (i, word) in words.enumerate() {
-        count = count + word.characters.count
-        // when we hit over the maxLength
-        if count + i - lineIndex > maxLength {
-            let wordsLength = count - word.characters.count
-            let spaceLength = maxLength - wordsLength
-            var eachLength = 1
-            var extraLength = 0
+
+    for (i, word) in words.enumerated() {
+        lineCharacterCount += word.count + 1
+        if lineCharacterCount >= maxLength {
+            let beginLineIndex = lineIndex
+            let endLineIndex = i - 1
+            lineIndex = i
             
-            let spaces = i - lineIndex - 1 // calculate the number of spaces
-            if spaces > 0 {
-                eachLength = spaceLength / spaces // calculate the number of spaces per space
-                extraLength = spaceLength % spaces // calculate the remainder space to distribute
+            // find the leftover space
+            let count = lineCharacterCount - (word.count + 2)
+            
+            lineCharacterCount = 0 // reset to ZERO
+            lineCharacterCount += word.count + 1 // begin counting for the next line
+            
+            // calculate the leftover to fill with spaces
+            var leftover = maxLength - count
+            print("leftover = \(leftover)")
+
+            // try to evenly distribute the spaces
+            let numberOfGaps = endLineIndex - beginLineIndex
+            print("numberOfGaps = \(numberOfGaps)")
+            var extraSpaces = 0
+            if numberOfGaps > 0 {
+                extraSpaces = leftover / numberOfGaps
             }
+            print("extraSpacesPerGap = \(extraSpaces)")
+            leftover = leftover - (extraSpaces * numberOfGaps)
+            print("leftover after = \(leftover)")
             
-            // start constructing the line and adding the words and spaces
-            var line = ""
-            
-            for k in lineIndex ..< i - 1 {
-                line += words[k]
-                
-                // add the number of spaces per space
-                var ce = 0
-                while ce < eachLength {
-                    line += " "
-                    ce += 1
+            // construct the line left and right justified
+            var text = ""
+            for word in words[beginLineIndex..<endLineIndex] {
+                text.append(word)
+                text.append(" ")
+                if leftover > 0 {
+                    leftover -= 1
+                    text.append(" ")
                 }
-                
-                // distribute the extra spaces
-                if extraLength > 0 {
-                    line += " "
-                    extraLength -= 1
-                }
+                let spaces = String(repeating: " ", count: extraSpaces)
+                text.append(spaces)
             }
+            text.append(words[endLineIndex])
+            print("line text = \(text)")
+            results.append(text)
+        }
+        
+        // handle last line
+        if i == words.endIndex - 1 {
+            let beginLineIndex = lineIndex
+            let endLineIndex = i
             
-            line += words[i - 1] // don't forget the last word in the line
-            
-            while line.characters.count < maxLength {
-                line += " "
+            var text = ""
+            for word in words[beginLineIndex..<endLineIndex] {
+                text.append(word)
+                text.append(" ")
             }
-            
-            results.append(line)
-            
-            lineIndex = i // set the start of the new lineIndex
-            count = words[i].characters.count
+            text.append(words[endLineIndex])
+            results.append(text)
         }
     }
-    
-    // handle the last line
-    var line = ""
-    
-    for i in lineIndex ..< words.count - 1 {
-        count = count + words[i].characters.count
-        line += words[i] + " "
-    }
-    
-    line += words.last!
-    while line.characters.count < maxLength {
-        line += " "
-    }
-    
-    results.append(line)
     return results
 }
 
 // let's test!
 let test1 = ["This", "is", "an", "example", "of", "text", "justification."]
-let result = textJustification(test1, maxLength: 16)
+let result1 = textJustification(words: test1, maxLength: 16)
+print(result1!)
+
+let test2 = ["This", "is", "an", "example", "of", "text", "justification."]
+let result2 = textJustification(words: test1, maxLength: 8)
+print(result2!)
+
+let test3 = ["This", "is", "an", "example", "of", "text", "justification."]
+let result3 = textJustification(words: test1, maxLength: 20)
+print(result3!)
