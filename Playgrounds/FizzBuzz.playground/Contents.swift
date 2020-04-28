b1d0275e3ed1518a4b53a20fdb0f57fbac439cf4
John Ngoi <johnnst@yahoo.com>
2020-04-27 21:26:19 -0700

    updated FizzBuzz.playground

--- Playgrounds/FizzBuzz.playground/Contents.swift
+++ Playgrounds/FizzBuzz.playground/Contents.swift
@@ -13,6 +13,8 @@
 // Example series for n = 16
 // 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16
 //
+// Time O(n)
+// Space O(1)
 
 func fizzBuzz (n: Int) -> String {
     var results = ""
